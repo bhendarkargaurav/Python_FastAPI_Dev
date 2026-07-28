6b30a6d9992b0361cbb2cbb98a120c5290990647
Gaurav <gaurav.bhe.24@gmail.com>
2026-07-28 19:58:54 +0530

    create task controller

---
+++ Task_Management_application/src/tasks/controller.py
@@ -0,0 +1,23 @@
+from fastapi import APIRouter, Depends
+from src.tasks.dtos import TaskSchema
+from sqlalchemy.orm import Session
+from src.tasks.models import TaskModel
+from src.utils.db import get_db
+
+def create_task(body: TaskSchema, db: Session):
+    data = body.model_dump()
+
+    new_task = TaskModel(
+        title=data["title"],
+        desciption=data["desciption"],
+        is_completed=data["is_completed"]
+    )
+
+    db.add(new_task)
+    db.commit()
+    db.refresh(new_task)
+
+    return {
+        "status": "Task Created Successfully..",
+        "data": new_task
+    }
